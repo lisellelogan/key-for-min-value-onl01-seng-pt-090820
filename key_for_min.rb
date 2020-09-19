1f63465188f3d33f9af18507e959f1c082662e90
Liselle Logan <liselle.logan@gmail.com>
2020-09-19 04:18:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, num|
-
+  name_hash.collect.with_index() do |name, num, index|
+    if num[index] < num[index + 1]
+      num
+    end
   end
 end
