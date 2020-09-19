7402fdf4074a1da49f8a2f8742748dc5c2f0ce1d
Liselle Logan <liselle.logan@gmail.com>
2020-09-19 04:55:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   values = 0
-  name_hash.collect do |name, num|
+  name_hash.collect.with_index() do |name, num, index|
     if num < name_hash[name]
       name
     end
